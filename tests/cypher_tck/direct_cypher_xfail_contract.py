4d8fd41f55434383611de083ceb007b040910395
Leo Meyerovich <leo@graphistry.com>
2026-04-27 14:35:35 -0700

    test(tck): classify expr-comparison3-1 as wrong-rows xfail

--- tests/cypher_tck/direct_cypher_xfail_contract.py
+++ tests/cypher_tck/direct_cypher_xfail_contract.py
@@ -23,6 +23,7 @@ DIRECT_CYPHER_XFAIL_VALUE_ERROR_KEYS: Final[tuple[str, ...]] = (
 
 DIRECT_CYPHER_XFAIL_WRONG_ROW_KEYS: Final[tuple[str, ...]] = (
     "expr-aggregation3-1",
+    "expr-comparison3-1",
     "expr-comparison1-6-5",
     "expr-comparison1-7-12",
     "expr-comparison1-7-13",
